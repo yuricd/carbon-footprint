b1f9b34bb2aa6470873fc64147f1364d5697af37
Yuri Delgado <yuricd@bol.com.br>
2021-04-18 12:15:54 -0300

    Fix Result test

--- frontend/src/components/Result/Result.test.tsx
+++ frontend/src/components/Result/Result.test.tsx
@@ -3,7 +3,8 @@ import { render } from '@testing-library/react'
 import { Result } from './Result'
 
 test('renders result', () => {
-  const { getByText } = render(<Result result={3} />)
+  const { getByText } = render(<Result category="some category" result={3} />)
   expect(getByText(/Your annual/i)).toBeInTheDocument()
   expect(getByText(/emission/i)).toBeInTheDocument()
+  expect(getByText(/some category/i)).toBeInTheDocument()
 })
